356c575a447105b08b0fd6436d7b786015bdb113
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-30 23:27:22 -0400

    Add #build_tree

--- tree.rb
+++ tree.rb
@@ -2,26 +2,30 @@ require_relative "node"
 
 class Tree
   
-  attr_accessor :input_array, :root
+  attr_accessor :root
 
   def initialize(input_array, root = nil)
-    @input_array = input_array.sort!.uniq!
+    @input_array = input_array
     @root = build_tree
   end
 
-  def build_tree
-    if base case to check if we're at the end of the tree, then return nil maybe?
-    
+  def build_tree(array = @input_array, start = 0, ending = (array.size - 1))
+    if start > ending
+      nil
     else 
-      root = middle element of the array
-      call build_tree recursively for the left side of the tree
-      call build_tree recursively for the right side of the tree
-      return root
-    
+      middle = (start + ending) / 2
+      root = Node.new(array[middle])
+      root.left_child = build_tree(array, start, middle - 1)
+      root.right_child = build_tree(array, middle + 1, ending)
+    end
+    root
   end
 
 end
 
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+test_array.sort!.uniq!
+p test_array
 tree = Tree.new(test_array)
-tree.build_tree
+root_node = tree.root
+p root_node.data_stored
