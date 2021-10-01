48d3a9ce7d46d61745b771f48a2ab1a56f7ced7e
Mikhail <mikhailgrigoriev92@gmail.com>
2021-10-01 08:12:40 -0400

    Add #insert

--- tree.rb
+++ tree.rb
@@ -6,26 +6,39 @@ class Tree
 
   def initialize(input_array, root = nil)
     @input_array = input_array
-    @root = build_tree
+    @root = build_tree(@input_array)
   end
 
-  def build_tree(array = @input_array, start = 0, ending = (array.size - 1))
-    if start > ending
+  def build_tree(array, start_index = 0, end_index = (array.size - 1))
+    if start_index > end_index
       nil
     else 
-      middle = (start + ending) / 2
-      root = Node.new(array[middle])
-      root.left_child = build_tree(array, start, middle - 1)
-      root.right_child = build_tree(array, middle + 1, ending)
+      middle_index = (start_index + end_index) / 2
+      p "node.data_stored == #{array[middle_index]}"
+      root = Node.new(array[middle_index])
+      root.left_child = build_tree(array, start_index, middle_index - 1)
+      root.right_child = build_tree(array, middle_index + 1, end_index)
     end
     root
   end
 
+  def insert(value, root)
+    if root.nil?
+      root = Node.new(value)
+    elsif value < root.data_stored
+      root.left_child = insert(value, root.left_child)
+    else
+      root.right_child = insert(value, root.right_child)
+    end
+    root
+  end
 end
 
-test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+test_array = [1, 2, 3, 4, 5, 6, 8]
 test_array.sort!.uniq!
-p test_array
+p "The test array is #{test_array}"
+p "A vertical view of the balanced binary search tree:"
 tree = Tree.new(test_array)
 root_node = tree.root
-p root_node.data_stored
+p "The level 0 root node has a value of #{root_node.data_stored}"
+tree.insert(7, root_node)
